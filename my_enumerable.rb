f87da30cca7ada997963d6d1348932eaa49c2c55
recillah khamala <recillahk@gmail.com>
2022-12-16 01:03:59 +0300

    add methods to the my_enumerable and my_list file.

---
+++ my_enumerable.rb
@@ -0,0 +1,27 @@
+module MyEnumerable
+    def all?
+      result = []
+      @list.each do |element|
+        result.push(element) if yield element
+      end
+      puts result.length == @list.length
+    end
+  
+    def any?
+      @list.each do |element|
+        if yield element
+          puts true
+          return true
+        end
+      end
+      puts false
+    end
+  
+    def filter
+      result = []
+      @list.each do |element|
+        result.push(element) if yield element
+      end
+      print result
+    end
+  end
