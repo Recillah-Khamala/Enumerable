ebd8b419cdfe64fd294e6be2bc944811a6e5b8d7
RichardChileya <richardchileyawk@gmail.com>
2022-12-16 13:56:51 +0200

    Trial fix

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -4,21 +4,24 @@ module MyEnumerable
     @list.each do |element|
       result.push(element) if yield element
     end
-      result.length == @list.length
+    puts result.length == @list.length
   end
 
   def any?
     @list.each do |element|
       if yield element
+        puts true
         return true
       end
     end
-
+    puts false
+  end
 
   def filter
     result = []
     @list.each do |element|
       result.push(element) if yield element
     end
+    print result
   end
-  
+end
