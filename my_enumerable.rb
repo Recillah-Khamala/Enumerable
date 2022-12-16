c4207cfab0c47ef148e9cc08da9c43090690be71
RichardChileya <richardchileyawk@gmail.com>
2022-12-16 12:13:04 +0200

    Fix linter error

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -4,24 +4,21 @@ module MyEnumerable
     @list.each do |element|
       result.push(element) if yield element
     end
-    puts result.length == @list.length
+      result.length == @list.length
   end
 
   def any?
     @list.each do |element|
       if yield element
-        puts true
         return true
       end
     end
-    puts false
-  end
+
 
   def filter
     result = []
     @list.each do |element|
       result.push(element) if yield element
     end
-    print result
   end
-end
+  
